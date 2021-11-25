0af7d6088817e10c386b0a0996484f54788bbac6
Ravinder <rkumarnirwal@gmail.com>
2021-11-25 19:52:34 +0530

    first commit

---
+++ tech/src/App.js
@@ -0,0 +1,22 @@
+import React,{useState} from 'react';
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from './components/NewExpense/NewExpense';
+
+let DUMMY_EXPENSE =[
+ 
+];
+let App=()=>{
+ const [expenses ,setExpenses]=useState(DUMMY_EXPENSE);
+const addExpenseHandler=(expense)=>{
+     const updatedExpense =[expense,...expenses];
+  setExpenses(updatedExpense);
+}
+  return(
+    <div>
+     <NewExpense onAddExpense={addExpenseHandler}/>
+     <Expenses item={expenses}/>
+    </div>
+  );
+}
+
+export default App;
